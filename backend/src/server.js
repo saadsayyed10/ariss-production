585c3c30f9e89ad9cd968ce943478ea23187be90
Saad Sayyed <saadsyed950@gmail.com>
2026-03-18 18:09:50 +0530

    Backend init

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server up and running on PORT: ${PORT}`);
+});
