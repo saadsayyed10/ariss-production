a24de8694774af2188e322d8312aa38b8ca5b811
Saad Sayyed <saadsyed950@gmail.com>
2026-03-18 18:13:18 +0530

    Env config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
+import { ENV } from "./config/env.config.js";
 
 const app = express();
-const PORT = 3001;
+const PORT = ENV.PORT;
 
 app.use(express.json());
 app.use(cors());
