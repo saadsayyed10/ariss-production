d38d325ff5499d7e79dccd1059ab8ec24bf46247
Saad Sayyed <saadsyed950@gmail.com>
2026-03-19 14:15:01 +0530

    Fetch profile route

--- backend/src/api/routes/admin.route.js
+++ backend/src/api/routes/admin.route.js
@@ -1,9 +1,16 @@
 import * as panelUserController from "../controllers/admin.controller.js";
 import { Router } from "express";
+import { protectPanelRoute } from "../../middleware/auth.middleware.js";
 
 const adminRouter = Router();
 
 adminRouter.post("/register", panelUserController.registerPanelUserController);
 adminRouter.post("/login", panelUserController.loginPanelUserController);
+
+adminRouter.get(
+  "/profile",
+  protectPanelRoute,
+  panelUserController.loginPanelUserController,
+);
 
 export default adminRouter;
